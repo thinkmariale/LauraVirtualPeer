ee44c792992b18946f2749e6467a16cd2c5d7938
mariale888 <monteblanco8@hotmail.com>
2014-04-16 22:46:23 -0400

    VP Animations
    VP integration with animations

--- Assets/Code/VPanimator.cs
+++ Assets/Code/VPanimator.cs
@@ -3,35 +3,67 @@ using System.Collections;
 
 public class VPanimator : MonoBehaviour {
 
+	public Sprite[] idle;
+	public Sprite[] angry;
+	public Sprite[] confused;
+	public Sprite[] frustrated;
+	public Sprite[] happy;
+	public Sprite[] sad;
+	public Sprite[] surprised;
+
 	public Sprite[] sprites;
 	public float framesPerSecond;
 
 	private SpriteRenderer spriteRenderer;
+	void Awake()
+	{
+
+	}
 	// Use this for initialization
 	void Start () {
 		spriteRenderer = renderer as SpriteRenderer;
+		addSprite(idle);
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		//int index = (int)(Time.timeSinceLevelLoad * framesPerSecond);
-		int index = 0;
+
+		int index = (int)(Time.timeSinceLevelLoad * framesPerSecond);
 		switch (WoOzChatLayer.emotion) {
+			case "angry":
+				addSprite(angry);
+				break;
+			case "confused":
+				addSprite(confused);
+				break;
+			case "frustrated":
+				addSprite(frustrated);
+				break;
 			case "happy":
-				index = 0;
+				addSprite(happy);
 				break;
 			case "sad":
-				index = 1;
+				addSprite(sad);
 				break;
 			case "excited":
-				index = 2;
+				addSprite(surprised);
 				break;
 			default:
-				index = 0;
+				addSprite(idle);
 				break;
 		}
-		
+
 		index = index % sprites.Length;
 		spriteRenderer.sprite = sprites[ index ];
+	}
+	void addSprite(Sprite[] x)
+	{
+	
+		for(int i=0;i<x.Length;i++)
+		{
+			sprites[i] = x[i];
+		}
+
 	}
 }
